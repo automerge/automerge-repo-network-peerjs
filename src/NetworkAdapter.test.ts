8b6ca783e00fa7d78f981cd6088f211c9d6eb759
Phil Cockfield <phil@cockfield.net>
2024-02-27 10:58:15 +1300

    placeholder test

--- src/NetworkAdapter.test.ts
+++ src/NetworkAdapter.test.ts
@@ -1,3 +1,7 @@
 import { describe, expect, it } from "vitest";
 
-describe("NetworkAdapter", () => {});
+describe("NetworkAdapter", () => {
+  it("placeholder", () => {
+    expect(123).to.eql(123);
+  });
+});
